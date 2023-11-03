80a481e48990fb3347e819723fc93c9fd044ffe2
rcatalangithub <148396098+rcatalangithub@users.noreply.github.com>
2023-11-03 11:16:10 -0300

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,8 +7,9 @@ public class App {
   
 
   public static String triangulo(int a, int b, int c) {
-    if (a == b) && (b == c) && (a == 1)
+    if (a == b) && (b == c) && (a == 1) {
       return "es un triangulo equilatero";
+    }
     return "error no es un triangulo";
   } 
   
